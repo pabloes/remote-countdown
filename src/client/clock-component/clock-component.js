896858b108f4bc90d9ba66a3d2fe4e5555a2dbcd
Pablo Estornut <estornut@gmail.com>
2017-05-14 17:32:59 +0200

    start with material

--- src/client/clock-component/clock-component.js
+++ src/client/clock-component/clock-component.js
@@ -4,14 +4,26 @@ export default {
   restrict: 'E',
   controller: clockController,
   controllerAs: 'clock',
-  template: '<div class="clock" ng-bind="clock.timeString" ng-class="{blink: clock.differenceInSeconds < 0}" ng-style="{color:clock.getTimeColor()}">88:88</div>' +
-    '<div id="main-controls" ng-if="clock.editable">' +
-      '<input ng-disabled="clock.paused || !clock.editable" id="button-start-1min" class="control control--time-button" type="button" ng-click="clock.onCountdown({seconds:60})" value="1 Min"/>' +
-      '<input ng-disabled="clock.paused || !clock.editable" id="button-start-5min" class="control control--time-button" type="button" ng-click="clock.onCountdown({seconds:5*60})" value="5 Min"/>' +
-  '<input id="text-custom-time" ng-disabled="clock.paused || !clock.editable" ng-model="model.typedTime" class="control" type="text" ng-init="model.typedTime = 10"/>' +
-    '<input ng-disabled="clock.paused || !clock.editable" id="button-start-custom" class="control control&#45;&#45;time-button control-time-button&#45;&#45;play" type="button" ng-click="clock.onCountdown({seconds:model.typedTime*60})"' +
-  ' value=">" />' +
-  '<input ng-disabled="!clock.editable" id="button-pause" type="button" ng-click="clock.isPaused()?clock.onResume():clock.onPause()" value="{{clock.isPaused()?\'resume\':\'pause\'}}" />' +
+  template:
+  '<div flex layout="row">' +
+  '<div flex class="clock" ng-bind="clock.timeString" ng-class="{blink: clock.differenceInSeconds < 0}" ng-style="{color:clock.getTimeColor()}">88:88</div>' +
+    '<div flex ng-if="clock.editable">' +
+      '<md-button flex="10" ' +
+  'ng-disabled="clock.paused || !clock.editable" ' +
+  ' class="control control--time-button" ' +
+  ' ng-click="clock.onCountdown({seconds:60})">1 m</md-button>' +
+      '<md-button flex="10" ng-disabled="clock.paused || !clock.editable" ng-click="clock.onCountdown({seconds:5*60})">5 m</md-button>' +
+
+    '<div layout="row" flex>' +
+  '<md-input-container flex>' +
+  '<label>Minutes</label>' +
+  '<input flex id="text-custom-time" ng-disabled="clock.paused || !clock.editable" ng-model="model.typedTime" class="control" type="text" ng-init="model.typedTime = 10"/></md-input-container>' +
+    '<md-button flex ng-disabled="clock.paused || !clock.editable" type="button" ng-click="clock.onCountdown({seconds:model.typedTime*60})"' +
+  ' >></md-button></div>' +
+
+
+  '<md-button flex="10" ng-disabled="!clock.editable" ng-click="clock.isPaused()?clock.onResume():clock.onPause()">{{clock.isPaused()?\'resume\':\'pause\'}}</md-button>' +
+  '</div>' +
   '</div>',
   bindings: {
     countdownData: '<',
--- src/client/main.js
+++ src/client/main.js
@@ -1,12 +1,10 @@
-import godmodoroClient from "./tempoteam";
 import * as angular from 'angular';
+import ngMaterial from 'angular-material';
 
 import connector from './connector/connector';
 import controller from './controller/controller';
 import clockModule from './clock-component/clock-module';
 
-angular.module('godmodoro', [clockModule])
+angular.module('godmodoro', [clockModule, ngMaterial])
     .controller('godmodoro', controller)
     .factory('connector', connector);
-
-window.gmc = godmodoroClient();

efad3acbe4a380f27129018fe8a8742c70211ec6
Pablo Estornut <estornut@gmail.com>
2017-05-23 09:36:52 +0200

    add clipboard

--- src/client/main.js
+++ src/client/main.js
@@ -1,10 +1,11 @@
 import * as angular from 'angular';
 import ngMaterial from 'angular-material';
+import clipboard from 'angular-clipboard';
 
 import connector from './connector/connector';
 import controller from './controller/controller';
 import clockModule from './clock-component/clock-module';
 
-angular.module('godmodoro', [clockModule, ngMaterial])
+angular.module('godmodoro', [clockModule, ngMaterial, clipboard.name])
     .controller('godmodoro', controller)
     .factory('connector', connector);

063fd6927302f0ec553a28ee625112b9e2d82a22
Pablo Estornut <estornut@gmail.com>
2017-06-16 21:33:11 +0200

    store_socket reducer

--- src/server/reducers.js
+++ src/server/reducers.js
@@ -1,9 +1,15 @@
-module.exports = function (state = {}, action) {
+
+module.exports = function (state = { socketCollection: [] }, action) {
   switch (action.type) {
     case 'STORE_SOCKET':
       return Object.assign({}, state, {
-        socketCollection: (state.socketCollection || []).concat([action.payload.socket]),
+        socketCollection: state.socketCollection.concat( [{
+          id:id++,
+          socket:action.payload.socket
+        }] )
       });
+      //return {...state, socketCollection: [...socketCollection, action.payload.socket]});
+
     default:
       return state;
   }
